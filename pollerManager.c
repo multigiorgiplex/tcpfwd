2b4b658499d3b90d9867eeb89e2c3d2d2012f1a6
multigiorgiplex <multigiorgiplex@users.noreply.github.com>
2019-08-24 17:35:43 +0200

    Deprecated ugly, unuseful and resource demanding linked list in pollerManager. Now a more efficient and clean solution to get the maximum fd from a fd_set (which is needed by PM_watchlist_remove()) is implemented.

--- pollerManager.c
+++ pollerManager.c
@@ -9,42 +9,49 @@
 #include <stdio.h>
 #endif
 
-struct _fd {	//internal use only
-	int fd;
-	struct _fd * next;
-};
-
 
 fd_set PM_watchlist[3];
 fd_set _PM_watchlist_compiled[3];
 int PM_watchlist_max_fd[3];
 struct timespec PM_timeout;
-struct _fd * fds[3];
 SH_signalMask PM_signalMask;		//Signals to avoid interrupting to when calling PM_watchlist_run()
 
 
-static void _PM_compile_watchlist (char list_type)	//internal
+/* returns the max fd in a specified set */
+static int _PM_get_max_fd (fd_set * set)
 {
-	struct _fd * list = fds[list_type];
+	/*	fd_set is just a struct containing an array of (__FD_SETSIZE / __NFDBITS) elements of __fd_mask type
+	 * 	__fd_mask is a integer type of length __NFDBITS bits, therefore the maximum fd allowed is __FD_SETSIZE.
+	 */
+	int ret = __FD_SETSIZE -1;
+	__fd_mask mask;
+	signed char i;
+	unsigned short j;
 
-	FD_ZERO (&_PM_watchlist_compiled[list_type]);
-	PM_watchlist_max_fd[list_type] = 0;
-
-#ifdef _DEBUG
-	printf ("\t%s: ", (list_type == PM_READ) ? "PM_READ" : "PM_WRITE");
-#endif
-	while (list)
+	/* for every element of fd_set.__fd_mask */
+	for (i = (__FD_SETSIZE / __NFDBITS) -1; i >= 0; i--)
 	{
-		FD_SET (list->fd, &_PM_watchlist_compiled[list_type]);
-		if (list->fd > PM_watchlist_max_fd[list_type]) PM_watchlist_max_fd[list_type] = list->fd;
-#ifdef _DEBUG
-		printf ("%d,", list->fd);
-#endif
-		list = list->next;
+		/* generate a bit mask starting a the leftmost bit */
+		mask = 1UL << (__NFDBITS -1);
+
+		/* for every bit in __fd_mask */
+		for (j = 0; j < __NFDBITS; j++)
+		{
+			/* compare if it matches with the mask */
+			if (set->__fds_bits[i] & mask)
+				/* if so, return the resulting fd */
+				return ret;
+			else
+			{
+				/* if not, right-shift the test mask by one and decrement the resulting fd */
+				mask >>= 1;
+				ret--;
+			}
+		}
 	}
-#ifdef _DEBUG
-	printf ("\b\n");
-#endif
+
+	/* if no fd found, return minus one */
+	return -1;
 }
 
 
@@ -52,89 +59,24 @@ void PM_init (unsigned watch_timeout)
 {
 	PM_timeout.tv_sec	= watch_timeout / 1000;
 	PM_timeout.tv_nsec	= (watch_timeout % 1000) * 1000000;
-	fds[PM_READ] = 0;
-	fds[PM_WRITE] = 0;
-	fds[PM_EXCEPT] = 0;
-	PM_watchlist_max_fd[PM_READ] = 0;
-	PM_watchlist_max_fd[PM_WRITE] = 0;
-	PM_watchlist_max_fd[PM_EXCEPT] = 0;
+	PM_watchlist_max_fd[PM_READ] = -1;
+	PM_watchlist_max_fd[PM_WRITE] = -1;
+	PM_watchlist_max_fd[PM_EXCEPT] = -1;
 	SH_clearSignalMask (&PM_signalMask);
 }
 
 
 void PM_watchlist_add (int fd, char list_type)
 {
-	struct _fd * curr;
-	struct _fd * prev;
-
-	if (fds[list_type] == 0)
-	{
-		fds[list_type] = malloc (sizeof (struct _fd));
-		if (!fds[list_type]) return;
-
-		fds[list_type]->fd		= fd;
-		fds[list_type]->next	= 0;
-
-		PM_watchlist_max_fd[list_type] = 0;
-		_PM_compile_watchlist (list_type);
-		return;
-	}
-
-	prev = fds[list_type];
-	curr = prev->next;	
-
-	while (curr != 0)
-	{
-		prev = curr;
-		curr = curr->next;
-	}
-	
-	curr = malloc (sizeof (struct _fd));
-	if (!curr)	return;
-
-	prev->next	= curr;
-	curr->fd	= fd;
-	curr->next	= 0;
-
-	PM_watchlist_max_fd[list_type] = 0;
-	_PM_compile_watchlist (list_type);
+	FD_SET (fd, &_PM_watchlist_compiled[list_type]);
+	if (fd > PM_watchlist_max_fd[list_type]) PM_watchlist_max_fd[list_type] = fd;
 }
 
 
 void PM_watchlist_remove (int fd, char list_type)
 {
-	struct _fd * curr;
-	struct _fd * prev = 0;
-
-	if (!fds[list_type])
-		return;
-
-	if (fds[list_type]->fd == fd)
-	{
-		curr = fds[list_type];	//temp
-		fds[list_type] = fds[list_type]->next;
-		free (curr);
-	}
-	else
-	{
-		prev = fds[list_type];
-		curr = fds[list_type]->next;
-
-		while (curr && curr->fd != fd)
-		{
-			prev = curr;
-			curr = curr->next;
-		}
-
-		if (curr)
-		{
-			prev->next = curr->next;
-			free (curr);
-		}
-	}
-	
-	PM_watchlist_max_fd[list_type] = 0;
-	_PM_compile_watchlist (list_type);
+	FD_CLR (fd, &_PM_watchlist_compiled[list_type]);
+	PM_watchlist_max_fd[list_type] = _PM_get_max_fd (&_PM_watchlist_compiled[list_type]);
 }
 
 
